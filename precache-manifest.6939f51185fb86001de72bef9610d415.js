33f6eddd871a11a5146495bf52e8f363c299593f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-24 15:58:52 -0500

    Updates

---
+++ precache-manifest.6939f51185fb86001de72bef9610d415.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f8812aa0ea26359cd2a",
+    "url": "/corfu-demo/static/js/main.6f8812aa.chunk.js"
+  },
+  {
+    "revision": "6ec212924568ac9f39fc",
+    "url": "/corfu-demo/static/css/1.db8cee4a.chunk.css"
+  },
+  {
+    "revision": "6ec212924568ac9f39fc",
+    "url": "/corfu-demo/static/js/1.6ec21292.chunk.js"
+  },
+  {
+    "revision": "7bc4ea19a7e1be787c06",
+    "url": "/corfu-demo/static/js/runtime~main.7bc4ea19.js"
+  },
+  {
+    "revision": "f582da9c8a2ebbe267cfd497d7ed4718",
+    "url": "/corfu-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9dcacc3a9b09492a364231477df1a827.js"
+  "/corfu-demo/precache-manifest.6939f51185fb86001de72bef9610d415.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/corfu-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

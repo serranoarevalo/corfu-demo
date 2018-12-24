b4c81419f3a14b6b420eee42c1fa5758af33b97f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-24 15:54:50 -0500

    Tags

--- src/Routes/Tags.js
+++ src/Routes/Tags.js
@@ -1,1 +1,35 @@
-export default () => "Tags";
+import React from "react";
+import styled from "styled-components";
+import { Tag } from "corfu";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  > div {
+    margin-bottom: 50px;
+  }
+`;
+
+const Title = styled.h1`
+  margin-bottom: 50px;
+  font-size: 32px;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  > * {
+    margin-right: 20px;
+  }
+`;
+
+export default () => (
+  <Container>
+    <Title>Tags</Title>
+    <Flex>
+      <Tag text={"Tag"} edges={"sharp"} bgColor={"primary"} />
+      <Tag text={"Another one"} edges={"rounded"} bgColor={"alert"} />
+      <Tag text={"This one"} edges={"round-sharp"} bgColor={"success"} />
+      <Tag text={"And this one"} edges={"rounded"} bgColor={"error"} />
+    </Flex>
+  </Container>
+);
